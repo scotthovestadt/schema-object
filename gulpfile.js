6fdf94ca66e516e8db22153af06d0bfbfdfcd85a
Scott Hovestadt <scott.hovestadt@gmail.com>
2015-11-03 19:04:04 -0800

    Added --grep option to gulp test and npm test commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var _gulp = require('load-plugins')('gulp-*');
+var argv = require('minimist')(process.argv.slice(2));
 
 gulp.task('test', ['build'], function() {
   return gulp.src('test/tests.js', { read: false })
-    .pipe(_gulp.mocha({reporter: 'spec'}));
+    .pipe(_gulp.mocha({
+      reporter: 'spec',
+      grep: argv.grep
+    }));
 });
 
 gulp.task('build', function() {
